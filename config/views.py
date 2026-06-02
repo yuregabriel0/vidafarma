a1d46a8fe142d55af801c816945732d885b7c55f
yuregabriel12 <yuregabriel360@gmail.com>
2026-06-01 22:33:36 -0300

    Implementa cadastro e pesquisa de produtos

--- config/views.py
+++ config/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect  #o render pega o html e entrega a requisicão // #o redirect manda o usuario para outra página
 from django.contrib.auth import authenticate, login, logout  
 from produtos.models import Produto
-from estoque.models import Lote, Movimentacao      #o render pega o html e entrega a requisicão // #o redirect manda o usuario para outra página
+from estoque.models import Lote, Movimentacao
+from produtos.models import Produto 
+from produtos.models import Produto, Categoria
 
 def login_view(request):
 
@@ -55,7 +57,60 @@ def produtos_view(request):
     if not request.user.is_authenticated:
         return redirect("login")
 
-    return render(request, "produtos.html")
+    pesquisa = request.GET.get("pesquisa")
+
+    if request.method == "POST":
+
+        categoria = Categoria.objects.get(
+            id_categoria=request.POST.get("categoria")
+        )
+
+        Produto.objects.create(
+            cod_prod=request.POST.get("cod_prod"),
+            nom_produto=request.POST.get("nom_produto"),
+            preco=request.POST.get("preco"),
+            qtd_est=0,
+            categoria=categoria
+        )
+
+        return redirect("produtos")
+
+    produtos = Produto.objects.select_related(
+        "categoria"
+    ).all()
+
+    if pesquisa:
+
+        produtos = produtos.filter(
+            nom_produto__icontains=pesquisa
+        )
+
+    categorias = Categoria.objects.all()
+
+    contexto = {
+        "produtos": produtos,
+        "categorias": categorias,
+
+        "total_produtos": Produto.objects.count(),
+
+        "produtos_ativos": Produto.objects.filter(
+            qtd_est__gt=0
+        ).count(),
+
+        "estoque_baixo": Produto.objects.filter(
+            qtd_est__lte=10
+        ).count(),
+
+        "produtos_inativos": Produto.objects.filter(
+            qtd_est=0
+        ).count(),
+    }
+
+    return render(
+        request,
+        "produtos.html",
+        contexto
+    )
 
     
 def estoque_view(request):
