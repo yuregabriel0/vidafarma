8beb0501e40c03799ca00178a3c4300426bb6c43
yuregabriel12 <yuregabriel360@gmail.com>
2026-06-03 23:27:00 -0300

    security: movendo secret key

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-11ekqpn^xibn96!*zg&c!q=#paucinyucz)zvf8&tq*2pspf^5"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
