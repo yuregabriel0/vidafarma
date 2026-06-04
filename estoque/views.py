5344aaafe7818b8fbb9d137852463a84b4ace333
yuregabriel12 <yuregabriel360@gmail.com>
2026-06-03 23:11:05 -0300

    chore: finalizacao do projeto vidafarma

--- estoque/views.py
+++ estoque/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, redirect  #o render pega o html e entrega a requisicão // #o redirect manda o usuario para outra página
-from django.contrib.auth import authenticate, login, logout  
-from produtos.models import Produto
-from estoque.models import Lote, Movimentacao
-from produtos.models import Produto 
-from produtos.models import Produto, Categoria
-from usuarios.models import Funcionario
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from datetime import date, timedelta
 from django.db.models import Q
 from django.contrib import messages
+
+from datetime import date, timedelta
+
+from produtos.models import Produto, Categoria
+from usuarios.models import Funcionario
+from estoque.models import Lote, Movimentacao
 
 def home_view(request):
 
